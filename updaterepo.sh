11fbfc9cf9f3e3c47a991c72b622b2f93eceff53
sandervanvugt <mail@sandervanvugt.nl>
2025-07-04 11:10:33 +0200

    message

--- updaterepo.sh
+++ updaterepo.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+mount /dev/sr0 /mnt || echo please connect the ISO file as CDRom drive in virtualbox
+
+# removing current read-only stuff
+umount /repo
+sed -i '/repo/' /etc/fstab
+
+# generating repo index files
 dnf install -y createrepo
 
 createrepo --update /repo/BaseOS
